ee07de75fedd5251e3c9ca2027847d2bad2f647d
syoungee <sunyoungah69@gmail.com>
2024-12-21 15:17:42 +0900

    InputText 클리어 버튼 추가

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -4,16 +4,31 @@ type InputTextProps = {
 };
 const model = defineModel<string>();
 defineProps<InputTextProps>();
+
+const clearInput = () => {
+  model.value = '';
+};
 </script>
 
 <template>
-  <label class="flex flex-col gap-4">
+  <label class="flex flex-col gap-4 relative">
     {{ label }}
-    <input
-      class="border-gray-400 border rounded-md h-10 px-4 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
-      type="text"
-      autocomplete="off"
-      v-model="model"
-    />
+    <div class="relative w-full">
+      <input
+        class="w-full border-gray-400 border rounded-md h-10 px-4 pr-10 hover:border-blue-400 invalid:border-red-500 focus:border-green-400 focus:outline-none"
+        type="text"
+        autocomplete="off"
+        v-model="model"
+      />
+      <!-- Clear button -->
+      <button
+        v-if="model"
+        @click="clearInput"
+        type="button"
+        class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600 focus:outline-none"
+      >
+        ✕
+      </button>
+    </div>
   </label>
 </template>
